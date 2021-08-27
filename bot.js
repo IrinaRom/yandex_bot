35fdafdd0a8437b45f21b7c7a42db684ef208453
Irina Romanova <showboarding@gmail.com>
2021-08-27 13:03:13 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,1 +1,34 @@
+// ==UserScript==
+// @name         Yandex bot
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Yandex bot
+// @author       Romanova Irina
+// @match        https://yandex.ru/
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
 
+let keywords = ["речные круизы", "круизы по России","volga cruise", "речной теплоход"];
+let keyword = keywords[getRandom(0,keywords.length)];
+let infoInput = document.querySelectorAll(".input__control")[0];
+let searchBtn = document.querySelectorAll(".button_theme_search")[0];
+infoInput.value = keyword;
+let links = document.links;
+
+if (searchBtn !== undefined) {
+    searchBtn.click();
+}else{
+    for (let i=0; i<links.length; i++) {
+        if (links[i].href.includes('infoflot.com')){
+            let link = links[i];
+            link.setAttribute("target", "_self");
+            link.click();           
+            console.log("Нашел строку" + links[i]);
+            break;
+        }
+    }
+}
+function getRandom(min,max) {
+return Math.floor(Math.random()*(max-min)+min)
+}
