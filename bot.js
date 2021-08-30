d49bb907dbf6f0093aac8114abf8dcf18fba22e9
Irina Romanova <showboarding@gmail.com>
2021-08-30 14:14:22 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,26 +9,55 @@
 // @grant        none
 // ==/UserScript==
 
-let keywords = ["речные круизы", "круизы по России","volga cruise", "речной теплоход"];
+let keywords = ["круизы по Нилу"];
 let keyword = keywords[getRandom(0,keywords.length)];
 let infoInput = document.querySelectorAll(".input__control")[0];
 let searchBtn = document.querySelectorAll(".button_theme_search")[0];
-infoInput.value = keyword;
+//infoInput.value = keyword;
 let links = document.links;
+let nextPage = document.querySelector(".pager__item_kind_next");
 
 if (searchBtn !== undefined) {
-    searchBtn.click();
+    let i = 0;
+    let timerId = setInterval(()=>{
+        infoInput.value += keyword[i];
+        i++;
+        if (i == keyword.length) {
+            clearInterval(timerId);
+            searchBtn.click();
+        }
+    },500);
+}else if(location.hostname == 'infoflot.com'){
+
+    setInterval(()=>{
+        let index = getRandom(0, links.length);
+        if(getRandom(0,101) >= 80) {
+        location.href = "https://www.yandex.ru";
+        }
+        if (links[index].href.indexOf("infoflot.com") !== -1)
+            links[index].click();}, getRandom(1000, 5000));
 }else{
+    let nextYandexPage = true;
     for (let i=0; i<links.length; i++) {
         if (links[i].href.includes('infoflot.com')){
             let link = links[i];
             link.setAttribute("target", "_self");
-            link.click();           
-            console.log("Нашел строку" + links[i]);
+            let nextYandexPage = false;
+            setTimeout(()=>{link.click();},getRandom(3000,7000));
+            console.log("Нашел строку " + links[i]);
             break;
         }
     }
+    if (document.querySelector('.pager__item_current_yes').innerText == "5") {
+        let nextYandexPage = false;
+        location.href = "https://www.yandex.ru";
+    }
+    if (nextYandexPage) {
+        setTimeout(()=>{nextPage.click();},getRandom(2000,6000));
+
+    }
 }
+
 function getRandom(min,max) {
-return Math.floor(Math.random()*(max-min)+min)
+    return Math.floor(Math.random()*(max-min)+min)
 }
